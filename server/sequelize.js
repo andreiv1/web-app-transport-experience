817da7ab3d134bd6b2abb794d1679bcb279895a3
andreiv1 <vasilieviciandrei01@gmail.com>
2022-11-26 19:04:48 +0200

    mysql support

--- server/sequelize.js
+++ server/sequelize.js
@@ -1,10 +1,32 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: "./sqlite/database.db"
-})
 
+let seq;
+switch (process.env.DB) {
+    case 'sqlite':
+        seq = new Sequelize({
+            dialect: process.env.DB,
+            storage: process.env.SQLITE_PATH
+        })
+        break;
+
+    case 'mysql':
+        seq = new Sequelize(
+            process.env.DB_NAME,
+            process.env.DB_USER,
+            process.env.DB_PASS,
+            {
+                host: process.env.DB_HOST,
+                dialect: process.env.DB
+            })
+        break;
+
+    default:
+        throw new Error("Choose between sqlite and mysql in .env file");
+}
+
+const sequelize = seq;
 sequelize.sync().then(() => {
     console.log('All models were synced successfully.')
 })

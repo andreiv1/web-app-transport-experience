3627f4ccf0c14a1dda2d366699017cdf7b239e5b
vilcuana <vilcuana20@stud.ase.ro>
2022-11-28 20:44:04 +0200

    Added remaining methods

--- server/models/user.js
+++ server/models/user.js
@@ -6,20 +6,40 @@ const User = sequelize.define("User", {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
+    allowNull: false
   },
   username: {
     type: DataTypes.STRING,
     unique: true,
+    allowNull: false,
+    validate:{
+      len:[6,32],
+      is: ["/^[0-9a-z]+$",'i']
+    }
+    
   },
   password: {
     type: DataTypes.STRING,
+    allowNull: false,
+    validate:{
+      len:[8,128],
+      is: ["/^[0-9a-zA-Z]+$",'i']
+    }
+    
   },
   email: {
-    type: DataTypes.STRING,
+    type: DataTypes.STRING(128),
     unique: true,
+    allowNull: false,
+    validate:{
+      isEmail:true,
+      isLowercase:true
+    }
   },
   enabled: {
     type: DataTypes.TINYINT(1),
+    allowNull: false,
+    defaultValue: false
   },
 });
 

f45b945cb43f8b1135905b83d07c6e7a7667e347
vilcuana <vilcuana20@stud.ase.ro>
2022-11-28 20:36:48 +0200

    Added remaining methods

--- server/routes/stops.js
+++ server/routes/stops.js
@@ -1,7 +1,9 @@
-let express = require('express')
+let express = require('express');
+const Line = require('../models/line');
+const Stop = require('../models/stop')
 let router = express.Router()
 
-const Stop = require('../models/stop')
+
 
 module.exports = router;
 
@@ -14,20 +16,60 @@ router.route('/add').post(async function (req, res) {
     }
 });
 
-router.route('/edit/:stopId').put(function (req, res) {
-    res.status(501).json({"message": "Not implemented - TO DO"});
+router.route('/edit/:stopId').put(async function (req, res) {
+    try {
+        let stop = await Stop.findByPk(req.params.stopId)
+        if (stop) {
+            stop = await stop.update(req.body)
+            res.status(200).json(stop)
+        }
+        else {
+            res.status(404).json({ "error": `stop with id ${req.params.stopId} not found` })
+        }
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
 });
 
-router.route('/delete/:stopId').delete(function (req, res) {
-    res.status(501).json({"message": "Not implemented - TO DO"});
+router.route('/delete/:stopId').delete(async function (req, res) {
+    try {
+        Stop.destroy({
+            where: {
+                id: req.params.stopId
+            }
+        }).then((rows) => {
+            if (rows === 1) {
+                res.status(200).json({ "message": `stop with id ${req.params.stopId} deleted successfully` })
+            } else {
+                res.status(404).json({ "error": `stop with id ${req.params.stopId} not found` })
+            }
+        })
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
 });
 
-router.route('/getAll').get(function (req, res) {
-    res.status(501).json({"message": "Not implemented - TO DO"});
+router.route('/getAll').get(async function (req, res) {
+    try {
+        let stops = await Stop.findAll();
+        res.status(200).json(stops);
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
 });
 
-router.route('/get/:stopId').get(function (req, res) {
-    res.status(501).json({"message": "Not implemented - TO DO"});
+router.route('/get/:stopId').get(async function (req, res) {
+    try {
+        let stop = await Stop.findByPk(req.params.stopId)
+        res.status(200).json(stop);
+    }
+    catch (err) {
+        res.status(500).json(err)
+    }
+
 });
 
 module.exports = router;

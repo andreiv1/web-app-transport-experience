8c1c684ddd91c86596e5d076dc51c57baf564025
vladubianca <vladubianca20@stud.ase.ro>
2022-11-29 23:23:45 +0200

    Added enum type

--- server/models/line.js
+++ server/models/line.js
@@ -1,31 +1,28 @@
-const sequelize = require('../sequelize');
-const { DataTypes } = require('sequelize')
+const sequelize = require("../sequelize");
+const { DataTypes } = require("sequelize");
 
-const Line = sequelize.define('Line',
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        vehicleType: {
-            type: DataTypes.TINYINT(1),
-            allowNull: false
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        startStopId: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        endStopId: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
-    });
-
+const Line = sequelize.define("Line", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  vehicleType: {
+    type: DataTypes.ENUM("BUS", "TROLLEYBUS", "TRAM", "SUBWAY"),
+    allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  startStopId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  endStopId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
 
 module.exports = Line;
-

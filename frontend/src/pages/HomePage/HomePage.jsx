e32984f9bed91f457a84ce5831ded3fec33d2ea6
vilcuana <vilcuana20@stud.ase.ro>
2023-01-03 17:32:13 +0200

    design HomePage

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -5,23 +5,30 @@ import { Stack, TextField } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import SearchIcon from '@mui/icons-material/Search';
-
+import "./HomePage.css";
 function HomePage() {
 
     return (
         <div>
             <Stack direction="row" spacing={2}>
-                <div className="searchBox">
-                    <TextField/>
-                    <Button variant="outlined"><SearchIcon/></Button>
+                <div id="searchBox" className="searchBox">
+                    <TextField 
+                    label="Search..."
+                    size="small"
+                    margin="normal"
+                    />
+                    <Button id="search"
+                    variant="outlined"
+                    size="small"
+                    ><SearchIcon/></Button>
                 </div>
-                <div className="buttons">
-                    <Button variant="outlined" startIcon={<AddCircleIcon />}>
+                <div id="buttons" className="buttons">
+                    <Button  id="btnAddExperience" variant="outlined" startIcon={<AddCircleIcon />}>
                         Add experience
                     </Button>
-                    {/* <Button variant="contained" endIcon={<SettingsIcon />}>
+                    <Button  id="btnAdmin"  variant="contained" endIcon={<SettingsIcon />}>
                         Admin Settings
-                    </Button> */}
+                    </Button> 
                 </div>
             </Stack>
             <ExperienceCard />

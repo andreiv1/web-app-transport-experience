fdfc5a660e4eec2d6e0033924f3f8c2a828ec762
vilcuana <vilcuana20@stud.ase.ro>
2023-01-03 17:31:53 +0200

    --design ExperienceCard

--- frontend/src/components/ExperienceCard/ExperienceCard.jsx
+++ frontend/src/components/ExperienceCard/ExperienceCard.jsx
@@ -4,38 +4,48 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-
 import Typography from '@mui/material/Typography';
+import PersonIcon from '@mui/icons-material/Person';
+import randomColor from "randomcolor";
+import "./ExperienceCard.css";
 
 function ExperienceCard() {
+    let color = randomColor();
     return (
-        <Card sx={{ maxWidth: 345 }}>
+        <Card id="experienceCard" sx={{ maxWidth: 345 }}>
             <CardHeader
                 avatar={
-                    <Avatar 
-                    // sx={{ bgcolor: red[500] }} 
-                    aria-label="recipe">
-                        U
+                    <Avatar
+                        sx={{ bgcolor: color }}
+                        aria-label="user">
+                        <PersonIcon />
                     </Avatar>
                 }
-                action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                    </IconButton>
-                }
+                // action={
+                //     <IconButton aria-label="settings">
+                //         {/* <MoreVertIcon /> */}
+                //     </IconButton>
+                // }
                 title="Username"
                 subheader="Date added"
             />
             <CardContent>
-                <Typography variant="body2" color="text.secondary">
+                <Typography variant="body2" color="text.secondary" align='center'>
+                    User .... travelled on line ......... from ...... to ... in ......... minutes.
+                </Typography>
+                <Typography variant="body2" color="text.secondary" align='center'>
                     User observations
+                </Typography>
+                <Typography variant="body2" color="text.secondary" align='left'>
+                    Crowdedness
+                </Typography>
+                <Typography variant="body2" color="text.secondary" align='left'>
+                    Satisfaction level
                 </Typography>
             </CardContent>
             <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
+                <IconButton aria-label="share"
+                sx={{ '&:hover': { color: 'blue'} }}>
                     <ShareIcon />
                 </IconButton>
             </CardActions>

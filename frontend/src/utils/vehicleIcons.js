4055bd12926d2e91ee358b7bbbe45a6084aa115c
andreiv1 <vasilievici.andrei01@gmail.com>
2023-01-07 20:58:25 +0200

    added function to get vehicle icon by type

---
+++ frontend/src/utils/vehicleIcons.js
@@ -0,0 +1,14 @@
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+import TramIcon from '@mui/icons-material/Tram';
+import SubwayIcon from '@mui/icons-material/Subway';
+
+export function getVehicleIcon(type) {
+    switch (type) {
+        case 'bus':
+            return <DirectionsBusIcon />
+        case 'tram':
+            return <TramIcon />
+        case 'subway':
+            return <SubwayIcon />
+    }
+}

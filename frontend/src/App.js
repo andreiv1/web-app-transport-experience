71cdfeb49055461605fddb2f332a86cd7389c74a
andreiv1 <vasilievici.andrei01@gmail.com>
2023-01-07 20:45:28 +0200

    Added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
-import AddExperienceForm from "./components/AddExperienceForm/AddExperienceForm";
-import ExperienceCard from "./components/ExperienceCard/ExperienceCard";
-import LoginForm from "./components/LoginForm/LoginForm";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import HomePage from "./pages/HomePage/HomePage";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import ResetPasswordPage from "./pages/ResetPasswordPage/ResetPasswordPage";
-import SignupPage from "./pages/SignupPage/SignupPage";
+import AddExperiencePage from './pages/AddExperiencePage.jsx'
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import ResetPasswordPage from './pages/ResetPasswordPage/ResetPasswordPage.jsx'
+import SignupPage from './pages/SignupPage/SignupPage.jsx';
+
+import Logout from './components/Logout.jsx';
+
+import Auth from './components/Auth.jsx';
+
 
 function App() {
   return (
-    <div>
-      {/* <HomePage/> */}
-      {/* <ExperienceCard /> */}
-      {/* <AddExperienceForm /> */}
-      <LoginPage />
-      {/* <SignupPage /> */}
-      {/* <ResetPasswordPage /> */}
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Navigate replace to='/home' />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/resetPassword" element={<ResetPasswordPage />} />
+        
+        <Route path="*" element={<NotFoundPage />} />
+
+        <Route element={<Auth />}>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/addExperience" element={<AddExperiencePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

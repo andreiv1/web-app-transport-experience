9a06efed46f02da9e527f936f0498322cae0c189
andreiv1 <vasilievici.andrei01@gmail.com>
2023-01-08 10:53:56 +0200

    Added navbar

--- frontend/src/pages/AddExperiencePage.jsx
+++ frontend/src/pages/AddExperiencePage.jsx
@@ -3,25 +3,42 @@ import React from "react";
 import {
     Card,
     CardContent,
-    Paper
+    Paper,
+    Grid
 } from "@mui/material";
 
+import NavBar from "../components/NavBar.jsx"
 import ExperienceForm from "../components/ExperienceForm.jsx"
-import BackgroundPage from "./BackgroundPage/BackgroundPage.jsx"
+import BackgroundPage, { getRandomImage } from "./BackgroundPage/BackgroundPage.jsx";
+
+import subway from "./BackgroundPage/bg-img/subway.jpg";
 
 function AddExperiencePage() {
-    return(
-        <BackgroundPage>
-        <Paper elevation={10}>
-            <Card>
-                <CardContent>
-                    {/* <Logo/> */}
-                    <ExperienceForm/>
-                </CardContent>
-            </Card>
-        </Paper>
-    </BackgroundPage>
-         
+    return (
+        <>
+            <NavBar />
+
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="top"
+                >
+                    <Paper elevation={10}>
+                        <Card>
+                            <CardContent>
+                                {/* <Logo/> */}
+                                <ExperienceForm />
+                            </CardContent>
+                        </Card>
+                    </Paper>
+                </Grid>
+ 
+
+
+
+        </>
+
     )
 }
 

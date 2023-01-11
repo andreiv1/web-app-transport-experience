b67f2f1ed07d0e10b71a732c0b4c059ef16258d2
andreiv1 <vasilieviciandrei01@gmail.com>
2023-01-11 13:42:33 +0000

    Removed nested middleware isUserAuth

--- backend/middleware/checkSessionUserId.js
+++ backend/middleware/checkSessionUserId.js
@@ -1,14 +1,12 @@
 const isUserAuth = require("./isUserAuth");
 
 const checkSessionUserId = (req, res, userId, next) => {
-    isUserAuth(req, res, () => {
-        if(req.userdata.id === userId) {
-            next();
-        }
-        else {
-            return res.status(403).json({ "error": "Not Authorized!" })
-        }
-    });
+    if(req.userdata.id === userId) {
+        next();
+    }
+    else {
+        return res.status(403).json({ "error": "Not Authorized!" })
+    }
 }
 
 module.exports = checkSessionUserId;

a0cf496db02c6c8678ba723d950f3963d1720c5c
andreiv1 <vasilievici.andrei01@gmail.com>
2023-01-11 22:52:07 +0200

    Added edit experience page

---
+++ frontend/src/pages/EditExperiencePage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import {
+    Card,
+    CardContent,
+    Paper,
+    Grid
+} from "@mui/material";
+
+import NavBar from "../components/NavBar.jsx"
+import ExperienceForm from "../components/ExperienceForm.jsx"
+
+function EditExperiencePage() {
+    return (
+        <>
+            <NavBar />
+
+            <Grid
+                container
+                direction="row"
+                justifyContent="center"
+                alignItems="top"
+            >
+                <Paper elevation={10} sx={{ maxWidth: 550, width: 600, mt: 3 }}>
+                    <Card>
+                        <CardContent>
+                            <ExperienceForm />
+                        </CardContent>
+                    </Card>
+                </Paper>
+            </Grid>
+
+
+
+
+        </>
+
+    )
+}
+
+export default EditExperiencePage
